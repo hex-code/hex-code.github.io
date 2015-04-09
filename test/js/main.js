bdc3e62d228e39568e01271472123ce8ec58d420
Yaroslav Sava <GrozaRazor@gmail.com>
2015-04-09 16:57:04 +0300

    add gallery

--- test/js/main.js
+++ test/js/main.js
@@ -3,6 +3,21 @@ $(document).ready(function() {
 		var link = $(this).attr('href');
 			activeSection(link);
 	});
+	$('.image-popup-no-margins').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		image: {
+			verticalFit: true,
+			cursor: null
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
 }());
 
 $(window).scroll(function() {
